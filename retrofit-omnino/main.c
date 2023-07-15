30704644ed535c2b5f2b690226dce4f3c835fdaa
Pedro Giló <phvg@ic.ufal.br>
2023-07-15 15:48:11 +0100

    Descoberto endereços dos motores: motor A = 255, motor B = 250, motor C = 245

--- retrofit-omnino/main.c
+++ retrofit-omnino/main.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <motor_control_driver.h>
 #include <common.h>
+#include <unistd.h>
 
 int main(){
 
@@ -11,8 +12,16 @@ int main(){
 
     int serial_port;
     sm_serial_motor_init(&serial_port);
+
     serial_motor motor_a = sm_set_serial_motor(&serial_port, 255);
+    serial_motor motor_b = sm_set_serial_motor(&serial_port, 250);
+    serial_motor motor_c = sm_set_serial_motor(&serial_port, 245);
+
     sm_set_velocity(&motor_a, 0, 's');
+    sm_set_velocity(&motor_b, 0, 's');
+    sm_set_velocity(&motor_c, 0, 's');
+
+    //sleep(1);
 
 #if DEBUG_MODE == 1
     printf("[main] The program has finished\n");
