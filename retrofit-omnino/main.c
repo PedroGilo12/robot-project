69696eb82cbce275ed99a0a09ea76c14f11a4f33
Pedro Giló <pedro.gilo@edge.ufal.br>
2023-07-14 23:28:45 -0300

    Adicionando main.c

--- retrofit-omnino/main.c
+++ retrofit-omnino/main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
 #include <wiringPi.h>
+#include <wiringSerial.h>
 
-int main() {
-	printf("Program has started...\n");
+int main ()
+{
+    int serial_port ;
+    char dat;
+    if ((serial_port = serialOpen ("/dev/serial0", 9600)) < 0)	/* open serial port */
+    {
+        fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+        return 1 ;
+    }
 
-	return 0;
+    if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
+    {
+        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+        return 1 ;
+    }
+
+    while(1){
+
+        if(serialDataAvail (serial_port) )
+        {
+            dat = serialGetchar (serial_port);		/* receive character serially*/
+            printf ("%c", dat) ;
+            fflush (stdout) ;
+            serialPutchar(serial_port, dat);		/* transmit character serially on port */
+        }
+    }
+
 }
